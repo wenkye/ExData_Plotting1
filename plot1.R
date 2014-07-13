793f07241641e4148d57bb971fd30862c1fc7034
Wen <wye0220@gmail.com>
2014-07-13 12:48:15 -0400

    add plot1 code

---
+++ plot1.R
@@ -0,0 +1,14 @@
+
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";",colClasses=c("character","character",rep("numeric",7)),na="?")
+
+df$Time <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
+df$Date <- as.Date(df$Date,"%d/%m/%Y")
+
+dates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+
+subData <- subset(df, Date %in% dates)
+
+hist(subData[,3], col="Red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+
+
